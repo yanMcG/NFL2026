ec9592f90323ef052fc2ba47e82f76188141b485
Ryan Mc Glynn <ryanmclgynn@gmail.com>
2026-03-19 15:09:10 +0000

    creatin components

---
+++ nfl2026/nfl2026/src/app/fixtures/fixtures.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fixtures',
+  standalone: true,
+  imports: [],
+  templateUrl: './fixtures.component.html',
+  styleUrl: './fixtures.component.css'
+})
+export class FixturesComponent {
+
+}
