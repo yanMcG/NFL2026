e0a6c74abfe88c7de60b26f2f6218a272b15b682
Ryan Mc Glynn <ryanmclgynn@gmail.com>
2026-03-19 15:27:45 +0000

    issues

--- nfl2026/nfl2026/src/app/app.component.ts
+++ nfl2026/nfl2026/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { FixturesComponent } from './fixtures/fixtures.component';
+import {RoutesComponent}from './routes/routes.component';
+import { ManagersComponent } from './managers/managers.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent,
+     FixturesComponent, RoutesComponent, 
+     ManagersComponent, TeamsComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- nfl2026/nfl2026/src/app/app.routes.ts
+++ nfl2026/nfl2026/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RoutesComponent } from './routes/routes.component';
+import { ManagersComponent } from './managers/managers.component';
+import { TeamsComponent } from './teams/teams.component';
+import { FixturesComponent } from './fixtures/fixtures.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : '', component: RoutesComponent, title: 'Home page'},
+    {path : '/managers', component: ManagersComponent, title: 'Managers page'},
+    {path : '/teams', component: TeamsComponent, title: 'Teams page'},
+    {path : '/fixtures', component: FixturesComponent, title: 'Fixtures Page'},
+];
+
+
