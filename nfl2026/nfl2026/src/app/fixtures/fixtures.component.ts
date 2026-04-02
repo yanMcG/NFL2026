f867c7c741af279615bf9f6344f7765016b12d0a
Ryan Mc Glynn <ryanmclgynn@gmail.com>
2026-04-02 19:11:07 +0100

    i have not figured it out yet but im getting close ot getting the colours of each team for the fixtures page .

--- nfl2026/nfl2026/src/app/fixtures/fixtures.component.ts
+++ nfl2026/nfl2026/src/app/fixtures/fixtures.component.ts
@@ -3,6 +3,10 @@ import { Fixture } from '../fixture';
 import { FixturesService } from '../fixtures.service';
 import {CommonModule} from '@angular/common';
 
+
+import { Team } from '../team';
+import { TeamsService } from '../teams.service';
+
 @Component({
   selector: 'app-fixtures',
   standalone: true,
@@ -10,9 +14,16 @@ import {CommonModule} from '@angular/common';
   templateUrl: './fixtures.component.html',
   styleUrl: './fixtures.component.css'
 })
+
+
 export class FixturesComponent {
   fixtures: Fixture[] = [];
   fixturesService = inject(FixturesService);
+
+  teams: Team[] = [];
+  teamService = inject(TeamsService);
+
+  teamColour:any[] = [];
 
   constructor(){
       this.fixturesService.getFixtures().subscribe(
@@ -20,6 +31,17 @@ export class FixturesComponent {
           this.fixtures = response;
         }
       );
+
   }
+  getTeamColour(){
+        this.teamService.getTeams().subscribe(
+        response => {
+          this.teams = response;
+          response.forEach((team: Team) => {
+            this.teamColour[team.id] = team.rgb;
+          });
+        }
+      );
+      }
 
 }
