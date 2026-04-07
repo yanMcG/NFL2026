78c466a09eb42e595848ba8c39dd96deb96239b1
Ryan Mc Glynn <ryanmclgynn@gmail.com>
2026-04-07 16:49:00 +0100

    setup the routerlink for every other page

--- nfl2026/nfl2026/src/app/app.routes.ts
+++ nfl2026/nfl2026/src/app/app.routes.ts
@@ -3,12 +3,22 @@ import { RoutesComponent } from './routes/routes.component';
 import { ManagersComponent } from './managers/managers.component';
 import { TeamsComponent } from './teams/teams.component';
 import { FixturesComponent } from './fixtures/fixtures.component';
+import { TeamRankingsComponent } from './team-rankings/team-rankings.component';
+import { ScoringStatsComponent } from './scoring-stats/scoring-stats.component';
+import { ScoringStats2Component } from './scoring-stats2/scoring-stats2.component';
+import { ScoringChartComponent } from './scoring-chart/scoring-chart.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path : '', component: RoutesComponent, title: 'home'},
     {path : 'managers', component: ManagersComponent, title: 'managers'},
     {path : 'teams', component: TeamsComponent, title: 'teams'},
     {path : 'fixtures', component: FixturesComponent, title: 'fixtures'},
+    {path : 'team-rankings', component: TeamRankingsComponent, title: 'team-rankings'},
+    {path : 'scoring-stats', component: ScoringStatsComponent, title: 'scoring-stats'},
+    {path : 'scoring-stats-ii', component: ScoringStats2Component, title: 'scoring-stats II'},
+    {path : 'scoring-chart', component: ScoringChartComponent, title: 'scoring-chart'},
+    {path : 'login', component: LoginComponent, title: 'login'}
 ];
 
 
