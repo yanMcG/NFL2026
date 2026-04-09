486a32eaf94371638ae9ad553f2607ee38906646
Ryan Mc Glynn <ryanmclgynn@gmail.com>
2026-04-09 14:09:34 +0100

    i cant seem to get the two tables connected

--- nfl2026/nfl2026/src/app/scoring-chart/scoring-chart.component.ts
+++ nfl2026/nfl2026/src/app/scoring-chart/scoring-chart.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Fixture } from '../fixture';
+import { FixturesService } from '../fixtures.service';
+import {CommonModule} from '@angular/common';
+
+
+import { Team } from '../team';
+import { TeamsService } from '../teams.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-scoring-chart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './scoring-chart.component.html',
   styleUrl: './scoring-chart.component.css'
 })
 export class ScoringChartComponent {
 
+    fixtures: Fixture[] = [];
+    fixturesService = inject(FixturesService);
+
+    teams: Team[] = [];
+    teamService = inject(TeamsService);
+
+    mergedArray: any[] = [];
+
+    //conactinate the two arrays
+
+    constructor(){
+        this.fixturesService.getFixtures().subscribe(
+          response => {
+            this.fixtures = response;
+          }
+        );
+  
+        this.teamService.getTeams().subscribe(
+          response => {
+            this.teams = response;
+          }
+        ); 
+        this.press();
+        //conactinate the two arrays https://youtu.be/9KGggRHysg0?si=-ccR9zOkJI4HimSO
+        //this.fixtures = this.fixtures.concat(this.teams);
+
+    }
+
+     press(){
+        this.mergedArray = [...this.fixtures, ...this.teams]
+        console.log("button pressed")
+        console.log('merged array:',this.mergedArray);
+        }
+  
 }
