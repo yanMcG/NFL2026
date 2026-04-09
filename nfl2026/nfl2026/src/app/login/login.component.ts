a62ce5d4d106345e9c4d060b00e6364605b0b886
Ryan Mc Glynn <ryanmclgynn@gmail.com>
2026-04-09 10:00:49 +0100

    i am displayimg fixutre values after chaning them but only in a string local version , need to work on chaniging the database

--- nfl2026/nfl2026/src/app/login/login.component.ts
+++ nfl2026/nfl2026/src/app/login/login.component.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {ReactiveFormsModule, FormControl, FormGroup, FormsModule} from '@angular/forms'
+
+import { Fixture } from '../fixture';
+import { FixturesService } from '../fixtures.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  isLoggedIn: boolean = false;
+  fixtures: Fixture[] = [];
+  fixturesService = inject(FixturesService);
+
+  isLoggedIn: boolean = true;
+
+  contactForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  updateForm = new FormGroup({
+    HomeTeamScore: new FormControl(''),
+    AwayTeamScore: new FormControl('')
+  });
+
+  constructor(){
+      this.fixturesService.getFixtures().subscribe(
+        response => {
+          this.fixtures = response;
+        }
+      );
+    }
+
+  //tommy the password is password, the email is thomas.devine@atu.ie
+  login(){
+    if(this.contactForm.value.email === "thomas.devine@atu.ie" && this.contactForm.value.password === "password"){
+      this.isLoggedIn = true;
+    }
+    else{
+      alert("Incorrect email or password. Please try again.");
+    }
+
+  }
+
+  submit(){
+    alert(this.contactForm.value.email)
+  }
+
+  updateFixture( id: number, awayTeamScore : string, homeTeamScore : string){
+    console.log('updating fixture scores')
+    console.log('Away Team Score:', awayTeamScore);
+    console.log('Home Team Score:', homeTeamScore);
+  }
 
 }
